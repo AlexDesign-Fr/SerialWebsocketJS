e77328d170d29fedb0416f64194e51456a597610
DR <None>
2021-02-23 19:34:17 +0100

    add mockup for sendSerial function

--- serialwebsocket.js
+++ serialwebsocket.js
@@ -2,10 +2,24 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 81 });
 
+
+
+
+function sendSerial(message) {
+  console.log(message);
+}
+
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    //console.log('received: %s', message);
+    sendSerial(message);
   });
 
   ws.send('Connected');
 });
+
+// TODO:
+// 1. Make configurable
+// 2. define decode function
+// 3. define serial event function
