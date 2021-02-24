3e2f7c8fcb255d2985a97d1d86270119ba7b6bb6
DR <None>
2021-02-24 20:14:07 +0100

    add ws broadcast

--- serialwebsocket.js
+++ serialwebsocket.js
@@ -2,24 +2,44 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 81 });
 
-
-
-
-function sendSerial(message) {
-  console.log(message);
-}
-
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    //console.log('received: %s', message);
-    sendSerial(message);
-  });
-
-  ws.send('Connected');
-});
-
 // TODO:
 // 1. Make configurable
 // 2. define decode function
 // 3. define serial event function
+
+
+// foreward WS event -> Serial Port
+// TODO: decode special commands
+function sendSerial(message) {
+  console.log(message);
+}
+
+// broadcast Serial Port -> all WS clients
+async function sendWS(message) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+// handle WS connection
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    sendSerial(message);
+  });
+  ws.send('Connected');
+});
+
+
+// Debug
+var dbg_cnt = 0;
+function dbg(){
+  console.log('Debug called');
+  sendWS(`Test ${dbg_cnt}`);
+  dbg_cnt++;
+  if (dbg_cnt < 90) {    
+    setTimeout(dbg, 3000);
+  }
+}
+dbg();
