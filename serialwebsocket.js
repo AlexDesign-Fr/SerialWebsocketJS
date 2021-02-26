68689ba870269a5af2b916d22e05e3d8d7d71523
DR <None>
2021-02-26 20:24:37 +0000

    add serial connection and forwarding

--- serialwebsocket.js
+++ serialwebsocket.js
@@ -1,17 +1,20 @@
 const WebSocket = require('ws');
+const SerialPort = require('serialport')
 
-const wss = new WebSocket.Server({ port: 81 });
+const wss = new WebSocket.Server({ port: 1024 });
+const port = new SerialPort('/dev/ttyACM0', {baudRate: 115200});
+
+const prnt_dbg = true;
 
 // TODO:
 // 1. Make configurable
 // 2. define decode function
-// 3. define serial event function
-
+// 3. Handle reconnect/connect during runtime
 
 // foreward WS event -> Serial Port
 // TODO: decode special commands
 function sendSerial(message) {
-  console.log(message);
+  port.write(message);
 }
 
 // broadcast Serial Port -> all WS clients
@@ -23,23 +26,26 @@ async function sendWS(message) {
   });
 }
 
+// handle available Serial data
+port.on('readable', function () {
+  dat = port.read()
+  sendWS(dat.toString());
+})
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
+
 // handle WS connection
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+  ws.on('message', function incoming(message) { 
     sendSerial(message);
   });
+  
+  ws.on('error', function(e){
+    console.log(`Received Error: ${e}`);
+  });
+
   ws.send('Connected');
 });
-
-
-// Debug
-var dbg_cnt = 0;
-function dbg(){
-  console.log('Debug called');
-  sendWS(`Test ${dbg_cnt}`);
-  dbg_cnt++;
-  if (dbg_cnt < 90) {    
-    setTimeout(dbg, 3000);
-  }
-}
-dbg();
