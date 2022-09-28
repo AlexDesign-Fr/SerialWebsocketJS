d238e930b2b7fe7e2c8d1c83432ed3d9bf8c1371
Alex <alex@alex-design.fr>
2022-09-28 23:59:51 +0200

    Add python script to start/stop fan

---
+++ ventilateur_ON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+'''
+Ce programme permet de mettre en route le ventilateur branché sur un relais 3V
+'''
+import time
+import RPi.GPIO as GPIO
+
+
+RELAIS_1_GPIO = 21
+
+if __name__ == '__main__':
+    # Definition des GPIO
+    GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
+    GPIO.setwarnings(False)
+    GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)  # GPIO Assign mode
+
+    print( "mise en route")
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # on
